7ec4ec90c560e531684d0648c73b3c55a09dcd97
tokamak <gaurav@raghuvanshy.com>
2022-03-09 18:11:06 +0100

    added env file to export default variables and modified minor script parameters

--- delegate.sh
+++ delegate.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# TODO gravity does not use config command use `--output json` flag for all commands where required
+# importing env vars for delegation
+# TODO create another env file and source it on another run so that we can delegate via 2 wallets using authz
+source ./env.sh
+
+echo $FROM_ADDRESS
 
 # env vars
 GRANTER=${GRANTER:-alice}
@@ -8,15 +12,21 @@ GRANTEE=${GRANTEE:-bob}
 BINARY=${BINARY:-simd}
 DENOM=${DENOM:-stake}
 MAX_AMOUNT=${MAX_AMOUNT}
+FROM_ADDRESS=${FROM_ADDRESS}
+DEFAULT_FLAGS='--chain-id gravity-bridge-3 --node https://gravitychain.io:26657'
 
-FROM_ADDRESS=$($BINARY keys show $GRANTER -a) # delegator address
+if [[ -z $FROM_ADDRESS ]]; then
+	FROM_ADDRESS=$($BINARY keys show $GRANTER -a $DEFAULT_FLAGS) # delegator address
+fi
+
 echo From Address $FROM_ADDRESS
 
 # withdraw rewards for delegator if exists
-$BINARY tx distribution withdraw-all-rewards --from $GRANTER -y
+$BINARY tx distribution withdraw-all-rewards --from $FROM_ADDRESS --generate-only $DEFAULT_FLAGS >./output/withdraw.json
+$BINARY tx authz exec output/withdraw.json --from $GRANTEE --fees 0$DENOM $DEFAULT_FLAGS --keyring-backend test -y
 
 # get account balance
-BALANCE=$($BINARY q bank balances $FROM_ADDRESS --output json | jq --arg DENOM $DENOM -r '.balances | map(select(.denom==$DENOM))[0].amount')
+BALANCE=$($BINARY q bank balances $FROM_ADDRESS --output json $DEFAULT_FLAGS | jq --arg DENOM $DENOM -r '.balances | map(select(.denom==$DENOM))[0].amount')
 echo "Balance: $BALANCE"
 
 # get total validators
@@ -28,6 +38,7 @@ echo Delegation Amount: $DELEGATION_AMOUNT
 
 if [[ ! -z $MAX_AMOUNT ]] && [[ $DELEGATION_AMOUNT > $MAX_AMOUNT ]]; then
 	DELEGATION_AMOUNT=$MAX_AMOUNT
+	echo DELEGATION_AMOUNT after MAX_AMOUNT check: $DELEGATION_AMOUNT
 fi
 
 # generate validators final json using py script
@@ -39,4 +50,4 @@ jq '.validators' output/unsigned.json
 # run authz tx commands for doing delegation
 # execute the output of gen.py as a payload for authz tx
 echo executing authz from grantee account: $GRANTEE
-$BINARY tx authz exec output/unsigned.json --from $GRANTEE --gas-prices 0uatom --fees 200$DENOM --fee-account $FROM_ADDRESS -y
+$BINARY tx authz exec output/unsigned.json --from $GRANTEE --gas auto --fees 0$DENOM $DEFAULT_FLAGS --keyring-backend test -y
